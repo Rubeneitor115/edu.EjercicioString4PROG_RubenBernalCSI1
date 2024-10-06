b9c24a4aef8218f271ea6572374e52b27483edfb
VIVOBOOKDERUBEN\ruben <rubenbernalramos@gmail.com>
2024-10-06 13:27:38 +0200

    no message

---
+++ edu.EjercicioString4PROG_RubenBernalCSI1/src/controladores/Inicial.java
@@ -0,0 +1,35 @@
+/**
+ * Rubén Bernal Ramos, CSI1
+ */
+
+package controladores;
+
+/**
+ * Clase controladora de la aplicación
+ * rbr - 061024
+ */
+public class Inicial {
+
+	/**
+	 * Método de entrada de la aplicación
+	 * rbr - 061024
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Variables
+		String primera, segunda;
+		
+		//Les asigno un valor a cada variable
+		primera = "Buenos";
+		segunda = "dias";
+		
+		//Concateno las variables y guardo la concatenación en una nueva variable
+		String tercera = primera + " " + segunda;
+		
+		//Imprimo por consola esta última variable
+		System.out.println(tercera);
+
+	}
+
+}
---
+++ edu.EjercicioString4PROG_RubenBernalCSI1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module edu.EjercicioString4PROG_RubenBernalCSI1 {
+}
